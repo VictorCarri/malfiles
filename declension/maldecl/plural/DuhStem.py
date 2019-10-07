b2b803ca25ccf4ca37ae1f70eb9b7b9af6881d02
Victor Carri <victorcarri@gmail.com>
2019-10-07 19:20:41 -0400

    Finished റ്-stem and ട്-stem plurals.
    Signed-off-by: Victor Carri <victorcarri@gmail.com>

--- declension/maldecl/plural/DuhStem.py
+++ declension/maldecl/plural/DuhStem.py
@@ -1,8 +1,8 @@
 from declension.maldecl import DeclensionGenerator
 
 class DuhStem(DeclensionGenerator):
-    def __init__(self):
-        pass
+    def genNominative(self, noun):
+        return noun + "\u0d15\u0d7e"
 
     def genAccusative(self, noun):
         return noun + "\u0d15\u0d33\u0d46"
@@ -20,4 +20,4 @@ class DuhStem(DeclensionGenerator):
         return noun + "\u0d15\u0d33\u0d4b\u0d1f\u0d4d"
 
     def genInstrumental(self, noun):
-        return noun.replace(u"\u0d1f\u0d4d", u"\u0d1f\u0d4d\u0d1f\u0d3f\u0d28\u0d3e\u0d7d")
+        return noun + "\u0d15\u0d33\u0d3e\u0d7d"
--- declension/maldecl/plural/RuhStem.py
+++ declension/maldecl/plural/RuhStem.py
@@ -1,8 +1,8 @@
 from declension.maldecl import DeclensionGenerator
 
 class RuhStem(DeclensionGenerator):
-    def __init__(self):
-        pass
+    def genNominative(self, noun):
+        return noun + "\u0d15\u0d7e"
 
     def genAccusative(self, noun):
         return noun + "\u0d15\u0d33\u0d46"
@@ -17,7 +17,7 @@ class RuhStem(DeclensionGenerator):
         return noun + "\u0d15\u0d33\u0d3f\u0d7d"
 
     def genSociative(self, noun):
-        return noun.replace(u"\u0d31\u0d4d", u"\u0d31\u0d4d\u0d31\u0d3f\u0d28\u0d4b\u0d1f\u0d4d")
+        return noun + "\u0d15\u0d33\u0d4b\u0d1f\u0d4d"
 
     def genInstrumental(self, noun):
-        return noun.replace(u"\u0d31\u0d4d", u"\u0d31\u0d4d\u0d31\u0d3f\u0d28\u0d3e\u0d7d")
+        return noun + "\u0d15\u0d33\u0d3e\u0d7d"
