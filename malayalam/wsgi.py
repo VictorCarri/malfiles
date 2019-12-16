36b40402d4b055553ab073195b29382d3a7a29f3
Victor Carri <victorcarri@gmail.com>
2019-12-16 00:12:31 -0500

    Tried to add venv modules to wsgi.py. It didn't work. Will try installing Django globally.
    Signed-off-by: Victor Carri <victorcarri@gmail.com>

--- malayalam/wsgi.py
+++ malayalam/wsgi.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 import sys
+
+sys.path.insert(0, os.path.abspath("../venv/lib/python3.6/site-packages")) # Add our virtual environment's site-packages to the path so that the WSGI modules will find Django
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))) # Add the project directory to Python's path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))) # Add the app directory to Python's path
+
 from django.core.wsgi import get_wsgi_application
-
-# Add the project directory to Python's path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
-# Add the app directory to Python's path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'malayalam.settings')
 application = get_wsgi_application()
