71ce166921a37773fbb88d5afa49f69dd6ad3e2b
Victor Carri <victorcarri@gmail.com>
2019-12-15 19:03:30 -0500

    Saving files before switching to a new branch.
    Signed-off-by: Victor Carri <victorcarri@gmail.com>

--- declension/views.py
+++ declension/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from declension.maldecl.decline import decline
+
+
 # The main index page
 def index(req=None):
     template = loader.get_template("declension/index.html")
@@ -10,7 +13,10 @@ def index(req=None):
 # Shows the full declension of a single noun
 def showDeclension(req=None, noun=""):
     template = loader.get_template("declension/noun.html")
-    ctx = {"noun": noun} # Pass the noun which was sent in
+    ctx = {
+        "noun": noun, # Pass the noun which was sent in
+        "declensions": decline(noun) # Pass the noun's declined forms
+    }
     return HttpResponse(template.render(ctx, req))
 
 def hello(req=None):
