1ba7a80e33b4877a5a105204652c074ab18bac60
Victor Carri <victorcarri@gmail.com>
2019-12-16 19:50:59 -0500

    Created plural decliner for aL-stems.
    Signed-off-by: Victor Carri <victorcarri@gmail.com>

---
+++ declension/maldecl/plural/ALStem.py
@@ -0,0 +1,23 @@
+from declension.maldecl.DeclensionGenerator import DeclensionGenerator
+
+class ALStem(DeclensionGenerator):
+    def genNominative(self, noun):
+        return noun + u"\u0d15\u0d7e"
+
+    def genAccusative(self, noun):
+        return noun + u"\u0d15\u0d33\u0d46"
+
+    def genGenitive(self, noun):
+        return noun + u"\u0d15\u0d33\u0d41\u0d1f\u0d46"
+
+    def genDative(self, noun):
+        return noun + u"\u0d15\u0d7e\u0d15\u0d4d\u0d15\u0d4d"
+
+    def genLocative(self, noun):
+        return noun + "\u0d15\u0d33\u0d3f\u0d7d"
+
+    def genSociative(self, noun):
+        return noun + "\u0d15\u0d33\u0d4b\u0d1f\u0d4d"
+
+    def genInstrumental(self, noun):
+        return noun + "\u0d15\u0d33\u0d3e\u0d7d"
