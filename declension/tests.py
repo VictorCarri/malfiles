cac778417d96a141f17c8f0c52731881f5485e1a
Victor Carri <victorcarri@gmail.com>
2020-01-02 17:45:03 -0500

    Added a test for the plural dative of aL-stems.
    Signed-off-by: Victor Carri <victorcarri@gmail.com>

--- declension/tests.py
+++ declension/tests.py
@@ -350,6 +350,11 @@ class ALStemTests(TestCase):
         eVal = "\u0d2a\u0d4a\u0d30\u0d41\u0d7e\u0d15\u0d33\u0d41\u0d1f\u0d46"  # /poɾuɭkaɭuʈe/
         self.assertEqual(declensions["Plural"]["Genitive"], eVal, "Expected {0} as the genitive plural of {1}, but received {2}".format(eVal, self.noun, declensions["Plural"]["Genitive"]))
 
+    def testPluralDative(self):
+        declensions = decline(self.noun)
+        eVal = "\u0d2a\u0d4a\u0d30\u0d41\u0d7e\u0d15\u0d7e\u0d15\u0d4d\u0d15\u0d4d" # /poɾuɭkaɭkːə/
+        self.assertEqual(declensions["Plural"]["Dative"], eVal, "Expected {0} as the dative plural of {1} but received {2}".format(eVal, self.noun, declensions["Plural"]["Dative"]))
+
 class SchwaStemTests(TestCase):
     def setUp(self):
         self.noun = "\u0d15\u0d24\u0d15\u0d4d" # കതക് (door)
